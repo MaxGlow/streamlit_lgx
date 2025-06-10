424d34c0f52c32f3ec3f7ab06c0aa1099302f356
10294 <liguoxi@bsdd.konicaminolta.jp>
2025-06-10 10:35:26 +0800

    222

--- first_app/uber_pickups.py
+++ first_app/uber_pickups.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Uber pickups in NYC111')
+st.title('Uber pickups in NYC1114444')
 
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/streamlit-demo-data/uber-raw-data-sep14.csv.gz')
